6c75bcabb6e61e4507030586fc42705f46949bee
danigb <danigb@87f8d446-a148-0410-9e96-a52f46714620>
2009-12-14 05:19:20 +0000

    more examples
    git-svn-id: https://emite.googlecode.com/svn/trunk/emite-lib@964 87f8d446-a148-0410-9e96-a52f46714620

--- src/com/calclab/emite/example/im/chat/client/ExampleIMChat.java
+++ src/com/calclab/emite/example/im/chat/client/ExampleIMChat.java
@@ -1,9 +1,20 @@
 package com.calclab.emite.example.im.chat.client;
 
+import com.calclab.emite.core.client.xmpp.session.Session;
+import com.calclab.emite.core.client.xmpp.session.Session.State;
+import com.calclab.emite.core.client.xmpp.stanzas.Message;
+import com.calclab.emite.im.client.chat.Chat;
+import com.calclab.emite.im.client.chat.ChatManager;
+import com.calclab.suco.client.Suco;
+import com.calclab.suco.client.events.Listener;
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.event.dom.client.ChangeEvent;
+import com.google.gwt.event.dom.client.ChangeHandler;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
+import static com.calclab.emite.core.client.xmpp.stanzas.XmppURI.*;
 
 public class ExampleIMChat implements EntryPoint{
 
@@ -12,8 +23,26 @@ public class ExampleIMChat implements EntryPoint{
     @Override
     public void onModuleLoad() {
 	panel = new VerticalPanel();
+	RootPanel.get("app").add(panel);
+
+	Session session = Suco.get(Session.class);
+	session.onStateChanged(new Listener<State>() {
+	    @Override
+	    public void onEvent(State state) {
+		log("Current state: " + state);
+	    }
+	});
+	
+	final ChatManager chatManager = Suco.get(ChatManager.class);
 	TextBox field = new TextBox();
 	panel.add(field);
+	field.addChangeHandler(new ChangeHandler() {
+	    @Override
+	    public void onChange(ChangeEvent event) {
+		Chat chat = chatManager.open(uri("test2@localhost"));
+		chat.send(new Message(event.toString()));
+	    }
+	});
 	
 	log("Example IM Chat");
     }
