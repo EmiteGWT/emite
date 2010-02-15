1962e28caaf3def8624facc51bb289ec66e032a2
vruiz.jurado@gmail.com <vruiz.jurado@gmail.com@87f8d446-a148-0410-9e96-a52f46714620>
2010-02-15 22:06:05 +0000

    Removed PrivateStorage error
    git-svn-id: https://emite.googlecode.com/svn/trunk/emite-lib@1225 87f8d446-a148-0410-9e96-a52f46714620

--- src/com/calclab/emite/xep/storage/client/PrivateStorageModule.java
+++ src/com/calclab/emite/xep/storage/client/PrivateStorageModule.java
@@ -1,14 +1,28 @@
 package com.calclab.emite.xep.storage.client;
 
 import com.calclab.emite.core.client.xmpp.session.Session;
+import com.calclab.suco.client.ioc.Decorator;
+import com.calclab.suco.client.ioc.Provider;
 import com.calclab.suco.client.ioc.decorator.Singleton;
 import com.calclab.suco.client.ioc.module.AbstractModule;
 import com.calclab.suco.client.ioc.module.Factory;
+import com.google.gwt.core.client.EntryPoint;
 
 /**
  * Implements XEP http://xmpp.org/extensions/xep-0049.html
  */
-public class PrivateStorageModule extends AbstractModule {
+public class PrivateStorageModule extends AbstractModule implements EntryPoint {
+
+    @Override
+    public void onModuleLoad() {
+    }
+
+    @Override
+    public <T> Provider<T> register(final Class<? extends Decorator> decoratorType, final Class<T> componentType,
+	    final Provider<T> provider) {
+	// TODO Auto-generated method stub
+	return null;
+    }
 
     @Override
     protected void onInstall() {
