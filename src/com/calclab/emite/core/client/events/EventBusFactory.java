261f9055d5cddbbf2994113fab2ba0555a968891
danigb <danigb@87f8d446-a148-0410-9e96-a52f46714620>
2010-08-19 10:20:46 +0000

    EventBusFactory bug removed. Fixes issue 272
    git-svn-id: https://emite.googlecode.com/svn/trunk/emite-lib@1585 87f8d446-a148-0410-9e96-a52f46714620

--- src/com/calclab/emite/core/client/events/EventBusFactory.java
+++ src/com/calclab/emite/core/client/events/EventBusFactory.java
@@ -1,27 +1,50 @@
 package com.calclab.emite.core.client.events;
 
+/**
+ * EventBuses are created using this factory. This allow us to change the
+ * default event bus (for example: different loggin or testing).
+ * 
+ * The default implementation (GwtEventBus) doesn't work outside GWT
+ * enviroments, so it should be replaced when unit testing
+ * 
+ */
 public class EventBusFactory {
 
+    /**
+     * A simple interface to create event buses
+     */
     public static interface Factory {
 	public EmiteEventBus create(String eventBusName);
     }
 
-    private static Factory factory;
+    /**
+     * The default factory creates a Gwt event bus.
+     */
+    private static Factory factory = new Factory() {
+	@Override
+	public EmiteEventBus create(String eventBusName) {
+	    return new GwtEmiteEventBus(eventBusName);
+	}
+    };
 
+    /**
+     * Create an event bus with the given name
+     * 
+     * @param eventBusName
+     *            the bus name: using for logging
+     * @return a new event bus
+     */
     public static EmiteEventBus create(String eventBusName) {
 	return EventBusFactory.factory.create(eventBusName);
     }
 
+    /**
+     * Changes the event bus factory
+     * 
+     * @param factory
+     */
     public static void setFactory(Factory factory) {
 	EventBusFactory.factory = factory;
     }
 
-    private EventBusFactory() {
-	EventBusFactory.factory = new Factory() {
-	    @Override
-	    public EmiteEventBus create(String eventBusName) {
-		return new GwtEmiteEventBus(eventBusName);
-	    }
-	};
-    }
 }
