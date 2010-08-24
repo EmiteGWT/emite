d7a1b55f88823938a90ac313d9a06a248e5a95bb
danigb <danigb@87f8d446-a148-0410-9e96-a52f46714620>
2010-08-24 08:01:37 +0000

    More error information. Fixes issue 109
    git-svn-id: https://emite.googlecode.com/svn/trunk/emite-lib@1596 87f8d446-a148-0410-9e96-a52f46714620

--- src/com/calclab/emite/core/client/events/ErrorEvent.java
+++ src/com/calclab/emite/core/client/events/ErrorEvent.java
@@ -1,11 +1,14 @@
 package com.calclab.emite.core.client.events;
 
 import com.calclab.emite.core.client.packet.IPacket;
+import com.calclab.emite.core.client.packet.MatcherFactory;
+import com.calclab.emite.core.client.packet.PacketMatcher;
 import com.google.gwt.event.shared.GwtEvent;
 import com.google.gwt.event.shared.HandlerRegistration;
 
 public class ErrorEvent extends GwtEvent<ErrorHandler> {
 
+    private static final PacketMatcher ERROR_STANZA_MATCHER = MatcherFactory.byName("error");
     private static final Type<ErrorHandler> TYPE = new Type<ErrorHandler>();
 
     public static HandlerRegistration bind(EmiteEventBus eventBus, ErrorHandler handler) {
@@ -14,11 +17,13 @@ public class ErrorEvent extends GwtEvent<ErrorHandler> {
     private final String errorType;
     private final String description;
     private final IPacket stanza;
+    private final IPacket errorStanza;
 
     public ErrorEvent(String errorType, String description, IPacket stanza) {
 	this.errorType = errorType;
 	this.description = description;
 	this.stanza = stanza;
+	this.errorStanza = stanza.getFirstChild(ERROR_STANZA_MATCHER);
     }
 
     @Override
@@ -28,6 +33,10 @@ public class ErrorEvent extends GwtEvent<ErrorHandler> {
 
     public String getDescription() {
 	return description;
+    }
+
+    public IPacket getErrorStanza() {
+	return errorStanza;
     }
 
     public String getErrorType() {
