f12047702b4eb11a9c1d6e916054ce66ec20eced
danigb <danigb@87f8d446-a148-0410-9e96-a52f46714620>
2010-12-09 20:49:15 +0000

    Better message filtering. Fixes issue 336
    git-svn-id: https://emite.googlecode.com/svn/trunk/emite-lib@1672 87f8d446-a148-0410-9e96-a52f46714620

--- src/com/calclab/emite/im/client/chat/PairChatSelectionStrategy.java
+++ src/com/calclab/emite/im/client/chat/PairChatSelectionStrategy.java
@@ -1,6 +1,8 @@
 package com.calclab.emite.im.client.chat;
 
+import com.calclab.emite.core.client.packet.MatcherFactory;
 import com.calclab.emite.core.client.packet.NoPacket;
+import com.calclab.emite.core.client.packet.PacketMatcher;
 import com.calclab.emite.core.client.xmpp.stanzas.BasicStanza;
 
 /**
@@ -10,20 +12,22 @@ import com.calclab.emite.core.client.xmpp.stanzas.BasicStanza;
  */
 public class PairChatSelectionStrategy implements ChatSelectionStrategy {
 
+    private final PacketMatcher mucMatcher = MatcherFactory.byNameAndXMLNS("x", "http://jabber.org/protocol/muc#user");
+
     @Override
     public ChatProperties extractProperties(final BasicStanza stanza) {
-	ChatProperties properties = new ChatProperties(stanza.getFrom());
-	boolean messageHasBody = stanza.getFirstChild("body") != NoPacket.INSTANCE;
-	String stanzaType = stanza.getAttribute("type");
-	// We don't support groupchat here.
-	boolean isGroupChatMessage = stanzaType != null && stanzaType.equals("groupchat");
-	properties.setShouldCreateNewChat(messageHasBody && !isGroupChatMessage);
-	return properties;
+        ChatProperties properties = new ChatProperties(stanza.getFrom());
+        boolean messageHasBody = stanza.getFirstChild("body") != NoPacket.INSTANCE;
+
+        boolean shouldCreate = messageHasBody && !(("groupchat").equals(stanza.getAttribute("type")));
+        shouldCreate = shouldCreate && (stanza.getFirstChildInDeep(mucMatcher) == NoPacket.INSTANCE);
+        properties.setShouldCreateNewChat(shouldCreate);
+        return properties;
     }
 
     @Override
     public boolean isAssignable(final ChatProperties chatProperties, final ChatProperties properties) {
-	return chatProperties.getUri().equalsNoResource(properties.getUri());
+        return chatProperties.getUri().equalsNoResource(properties.getUri());
     }
 
 }
--- test/com/calclab/emite/im/client/chat/PairChatSelectionStrategyTests.java
+++ test/com/calclab/emite/im/client/chat/PairChatSelectionStrategyTests.java
@@ -9,7 +9,9 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.calclab.emite.core.client.packet.IPacket;
 import com.calclab.emite.core.client.xmpp.stanzas.Message;
+import com.calclab.emite.xtesting.services.TigaseXMLService;
 
 public class PairChatSelectionStrategyTests {
 
@@ -17,35 +19,46 @@ public class PairChatSelectionStrategyTests {
 
     @Before
     public void beforeTests() {
-	strategy = new PairChatSelectionStrategy();
+        strategy = new PairChatSelectionStrategy();
     }
 
     @Test
     public void shouldAssignToSameIfResourceChanged() {
-	ChatProperties chatProperties = new ChatProperties(uri("user@domain"));
-	assertTrue(strategy.isAssignable(chatProperties, new ChatProperties(uri("user@domain"))));
-	assertTrue(strategy.isAssignable(chatProperties, new ChatProperties(uri("user@domain/res1"))));
-	assertTrue(strategy.isAssignable(chatProperties, new ChatProperties(uri("user@domain/res2"))));
+        ChatProperties chatProperties = new ChatProperties(uri("user@domain"));
+        assertTrue(strategy.isAssignable(chatProperties, new ChatProperties(uri("user@domain"))));
+        assertTrue(strategy.isAssignable(chatProperties, new ChatProperties(uri("user@domain/res1"))));
+        assertTrue(strategy.isAssignable(chatProperties, new ChatProperties(uri("user@domain/res2"))));
     }
 
     @Test
     public void shouldExtractFromProperty() {
-	Message message = new Message("body", uri("recipient@domain"), uri("sender@domain"));
-	ChatProperties properties = strategy.extractProperties(message);
-	assertNotNull(properties);
-	assertEquals(uri("sender@domain"), properties.getUri());
+        Message message = new Message("body", uri("recipient@domain"), uri("sender@domain"));
+        ChatProperties properties = strategy.extractProperties(message);
+        assertNotNull(properties);
+        assertEquals(uri("sender@domain"), properties.getUri());
     }
 
     @Test
     public void shouldInitiateCreationWhenMessageBody() {
-	ChatProperties properties = strategy.extractProperties(new Message("body"));
-	assertTrue(properties.shouldCreateNewChat());
+        ChatProperties properties = strategy.extractProperties(new Message("body"));
+        assertTrue(properties.shouldCreateNewChat());
+    }
+
+    /* Based on real facts ;) */
+    @Test
+    public void shouldNotInitiateCreationIfMessageHasInvitation() {
+        IPacket stanza = TigaseXMLService.toPacket("<message to='test1@localhost' " + "from='room@conference.localhost' xmlns='jabber:client' "
+                + "type='normal'><x xmlns='http://jabber.org/protocol/muc#user'>" + "<invite from='test1@localhost/emite-1291918896669'><reason />"
+                + "</invite></x><x jid='room@conference.localhost' " + "xmlns='jabber:x:conference' />"
+                + "<body>test1@localhost/emite-1291918896669 invites you to the room room@conference.localhost</body></message>");
+        ChatProperties properties = strategy.extractProperties(new Message(stanza));
+        assertFalse(properties.shouldCreateNewChat());
     }
 
     @Test
     public void shouldNotInitiateCreationWhenNotBody() {
-	ChatProperties properties = strategy.extractProperties(new Message((String) null));
-	assertFalse(properties.shouldCreateNewChat());
+        ChatProperties properties = strategy.extractProperties(new Message((String) null));
+        assertFalse(properties.shouldCreateNewChat());
     }
 
 }
