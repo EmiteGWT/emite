d892519a00fd93903c42c7243cd9db3bdcc8ff48
ashward.surevine <ashward.surevine@87f8d446-a148-0410-9e96-a52f46714620>
2010-12-21 17:55:42 +0000

    Updated RosterItemsOrder.byName to order items consistently (was causing oddities if getName returned null for only some items) and to ignore case in the compare.
    git-svn-id: https://emite.googlecode.com/svn/trunk/emite-lib@1679 87f8d446-a148-0410-9e96-a52f46714620

--- src/com/calclab/emite/im/client/roster/RosterItemsOrder.java
+++ src/com/calclab/emite/im/client/roster/RosterItemsOrder.java
@@ -5,81 +5,79 @@ import java.util.Comparator;
 public class RosterItemsOrder {
 
     public static final Comparator<RosterItem> byName = new Comparator<RosterItem>() {
-	@Override
-	public int compare(final RosterItem item1, final RosterItem item2) {
-	    if (item1.getName() != null && item2.getName() != null) {
-		return item1.getName().compareTo(item2.getName());
-	    } else {
-		return item1.getJID().toString().compareTo(item2.getJID().toString());
-	    }
+        @Override
+        public int compare(final RosterItem item1, final RosterItem item2) {
+            final String item1Name = item1.getName() == null ? item1.getJID().toString() : item1.getName();
+            final String item2Name = item2.getName() == null ? item2.getJID().toString() : item2.getName();
 
-	}
+            return item1Name.compareToIgnoreCase(item2Name);
+        }
     };
 
     /**
      * Available first. Inside availables, items with getShow() == Show.dnd last
      */
     public static final Comparator<RosterItem> byAvailability = new Comparator<RosterItem>() {
-	@Override
-	public int compare(final RosterItem item1, final RosterItem item2) {
-	    final boolean av1 = item1.isAvailable();
-	    final boolean av2 = item2.isAvailable();
+        @Override
+        public int compare(final RosterItem item1, final RosterItem item2) {
+            final boolean av1 = item1.isAvailable();
+            final boolean av2 = item2.isAvailable();
 
-	    if (av1 && !av2) {
-		return -1;
-	    } else if (!av1 && av2) {
-		return 1;
-	    } else if (av1 && av2) {
-		return getShowLevel(item2) - getShowLevel(item1);
-	    } else {
-		return 0;
-	    }
+            if (av1 && !av2) {
+                return -1;
+            } else if (!av1 && av2) {
+                return 1;
+            } else if (av1 && av2) {
+                return getShowLevel(item2) - getShowLevel(item1);
+            } else {
+                return 0;
+            }
 
-	}
+        }
 
-	private int getShowLevel(final RosterItem item) {
-	    switch (item.getShow()) {
-	    case dnd:
-		return 1;
-	    case away:
-	    case xa:
-		return 2;
-	    default:
-		return 3;
-	    }
-	}
+        private int getShowLevel(final RosterItem item) {
+            switch (item.getShow()) {
+            case dnd:
+                return 1;
+            case away:
+            case xa:
+                return 2;
+            default:
+                return 3;
+            }
+        }
     };
 
     public static Comparator<RosterItem> groupedFirst = new Comparator<RosterItem>() {
-	@Override
-	public int compare(final RosterItem item1, final RosterItem item2) {
-	    final Integer item1Size = item1.getGroups().size();
-	    final Integer item2Size = item2.getGroups().size();
+        @Override
+        public int compare(final RosterItem item1, final RosterItem item2) {
+            final Integer item1Size = item1.getGroups().size();
+            final Integer item2Size = item2.getGroups().size();
 
-	    if (((item1Size == 0) && (item2Size == 0)) || ((item1Size > 0) && (item2Size > 0))) {
-		return 0;
-	    } else if (item1Size == 0) {
-		return 1;
-	    } else {
-		return -1;
-	    }
-	}
+            if (((item1Size == 0) && (item2Size == 0)) || ((item1Size > 0) && (item2Size > 0))) {
+                return 0;
+            } else if (item1Size == 0) {
+                return 1;
+            } else {
+                return -1;
+            }
+        }
     };
 
     public static Comparator<RosterItem> order(final Comparator<RosterItem>... comparators) {
-	return new Comparator<RosterItem>() {
-	    @Override
-	    public int compare(final RosterItem o1, final RosterItem o2) {
-		int result;
-		for (final Comparator<RosterItem> order : comparators) {
-		    result = order.compare(o1, o2);
-		    if (result != 0) {
-			return result;
-		    }
-		}
-		return 0;
-	    }
-	};
+        return new Comparator<RosterItem>() {
+            @Override
+            public int compare(final RosterItem o1, final RosterItem o2) {
+                int result;
+                for (final Comparator<RosterItem> order : comparators) {
+                    result = order.compare(o1, o2);
+                    if (result != 0) {
+                        return result;
+                    }
+                }
+                return 0;
+            }
+        };
     }
 
 }
